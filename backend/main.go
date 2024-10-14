82f0f1c59c8a343678ebdd741ab0ab553175a64a
nakamuraitsuki <isukin@gmail.com>
2024-10-14 23:11:33 +0900

    データベースUsers定義

--- backend/main.go
+++ backend/main.go
@@ -9,6 +9,17 @@ import (
 	_"github.com/mattn/go-sqlite3"
 )
 
+const (
+	/*ユーザーテーブル作成SQL*/
+	createUserTable = `
+		CREATE TABLE IF NOT EXISTS users(
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL,
+			pw_hash TEXT NOT NULL
+		)
+	`
+)
+
 func init(){
 	db, err := sql.Open("sqlite3","./database.db")
 	if err != nil{
@@ -20,12 +31,19 @@ func init(){
 }
 
 func main(){
+	/*データベース接続*/
 	db, err := sql.Open("sqlite3", "./database.db")
 	if err != nil {
 		log.Fatal(err)
 		panic(err);
 	}
 	defer db.Close()
+
+	/*テーブル作成*/
+	_, err = db.Exec(createUserTable)
+	if err != nil {
+		panic(err)
+	}
 
 	// サーバーの起動、ポート番号は8080
 	fmt.Println("http://localhost:8080 でサーバーを起動します")
